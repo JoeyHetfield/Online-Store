2756c5b5b32871b63041b6172f3fbb2c1ef8dd24
pedrojaoa <joaopfacbc@gmail.com>
2022-11-09 18:40:19 -0300

    Requisito 3 [AJUSTADO]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
 import Cart from './components/Cart';
 import Home from './components/Home';
+import './App.css';
 
 function App() {
   return (
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,8 +8,6 @@ class Home extends Component {
       <div data-testid="home-initial-message">
         Digite algum termo de pesquisa ou escolha uma categoria.
         <ProductsList />
-        <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
-        <button data-testid="shopping-cart-button" type="button">botao</button>
         <Botao />
       </div>
     );
