801be838d2f074f5967d9f011b31763ddf012837
Joey Hetfield <joeydefenix@hotmail.com>
2022-11-09 15:34:38 -0300

    Requisito 2

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </div>
+    );
+  }
+}
+
+export default Login;
