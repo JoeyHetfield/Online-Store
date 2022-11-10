4b0529ebbc89e06fb97d96aa56e9a7a9e161cd0f
Joey Hetfield <joeydefenix@hotmail.com>
2022-11-10 15:24:35 -0300

    Requisito 6

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { getCategories } from '../services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class ProductsList extends Component {
   state = {
     lista: [],
+    listaProdutos: [],
   };
 
   async componentDidMount() {
@@ -11,28 +12,53 @@ class ProductsList extends Component {
     this.setState({ lista: allCategories });
   }
 
+  handleChange = async (event) => {
+    const { id } = event.target;
+    const list = await getProductsFromCategoryAndQuery(id);
+    this.setState({
+      listaProdutos: list.results,
+    });
+  };
+
   render() {
-    const { lista } = this.state;
+    const { lista, listaProdutos } = this.state;
+    const listaDeProdutos = listaProdutos.map((product, index) => (
+      <li
+        key={ index }
+        data-testid="product"
+      >
+        {product.title}
+        <img
+          src={ product.thumbnail }
+          alt={ product.title }
+        />
+        {product.price}
+      </li>
+    ));
     return (
-      <ul>
-        {
-          lista.map((category) => (
-            <li id="liProducts" key={ category.id }>
-              <label
-                data-testid="category"
-                htmlFor={ category.id }
-              >
-                <input
-                  type="radio"
-                  id={ category.id }
-                  name="category"
-                />
-                {category.name}
-              </label>
-            </li>
-          ))
-        }
-      </ul>
+      <div>
+        <ul>
+          {
+            lista.map((category) => (
+              <li id="liProducts" key={ category.id }>
+                <label
+                  data-testid="category"
+                  htmlFor={ category.id }
+                >
+                  <input
+                    type="radio"
+                    id={ category.id }
+                    name="category"
+                    onChange={ this.handleChange }
+                  />
+                  {category.name}
+                </label>
+              </li>
+            ))
+          }
+        </ul>
+        { listaDeProdutos }
+      </div>
     );
   }
 }
